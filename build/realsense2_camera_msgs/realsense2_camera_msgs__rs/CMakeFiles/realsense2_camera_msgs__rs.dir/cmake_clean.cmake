8bc9e1eed8e765f5ecb8bf8b332445138584050a
fredRocs <frederic.giraud@balgrist.ch>
2026-06-05 12:30:57 +0200

    Reaorganized and compatible with run_dev

---
+++ build/realsense2_camera_msgs/realsense2_camera_msgs__rs/CMakeFiles/realsense2_camera_msgs__rs.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "/workspaces/isaac_ros-dev/build/realsense2_camera_msgs/rosidl_generator_rs/realsense2_camera_msgs/rust/Cargo.toml"
+  "/workspaces/isaac_ros-dev/build/realsense2_camera_msgs/rosidl_generator_rs/realsense2_camera_msgs/rust/build.rs"
+  "/workspaces/isaac_ros-dev/build/realsense2_camera_msgs/rosidl_generator_rs/realsense2_camera_msgs/rust/src/lib.rs"
+  "/workspaces/isaac_ros-dev/build/realsense2_camera_msgs/rosidl_generator_rs/realsense2_camera_msgs/rust/src/msg.rs"
+  "/workspaces/isaac_ros-dev/build/realsense2_camera_msgs/rosidl_generator_rs/realsense2_camera_msgs/rust/src/msg/rmw.rs"
+  "/workspaces/isaac_ros-dev/build/realsense2_camera_msgs/rosidl_generator_rs/realsense2_camera_msgs/rust/src/srv.rs"
+  "/workspaces/isaac_ros-dev/build/realsense2_camera_msgs/rosidl_generator_rs/realsense2_camera_msgs/rust/src/srv/rmw.rs"
+  "CMakeFiles/realsense2_camera_msgs__rs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/realsense2_camera_msgs__rs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/realsense2_camera_msgs/realsense2_camera_msgs__rs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/isaac_ros-dev/build/realsense2_camera_msgs/realsense2_camera_msgs__rs
+# Build directory: /workspaces/isaac_ros-dev/build/realsense2_camera_msgs/realsense2_camera_msgs__rs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
